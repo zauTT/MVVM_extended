ffacc9b94429c4037bf6570ce2b1b0881ada5ada
zauTT <giozau1@gmail.com>
2025-03-25 20:35:25 +0400

    Initial Commit

---
+++ try2_lecture19_GiorgiZautashvili_MVVM_extended/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  try2_lecture19_GiorgiZautashvili_MVVM_extended
+//
+//  Created by Giorgi Zautashvili on 25.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
